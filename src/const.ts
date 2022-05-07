cd64b72cac31543037c15456533c6a15eff7623e
Юрий <y.headman@ya.ru>
2022-05-07 17:09:07 +0300

    minor changes

--- src/const.ts
+++ src/const.ts
@@ -3,25 +3,3 @@ export enum AppRoute {
   Comments = '/comments',
   User = '/user',
 }
-
-// "phone": "1-770-736-8031 x56442",
-//   "website": "hildegard.org",
-//   "company": {
-//   "name": "Romaguera-Crona",
-//     "catchPhrase": "Multi-layered client-server neural-net",
-//     "bs": "harness real-time e-markets"
-// }
-// },
-// "Post": {
-//   "userId": 1,
-//     "id": 1,
-//     "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-//     "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
-// },
-// "Comment": {
-//   "postId": 1,
-//     "id": 2,
-//     "name": "quo vero reiciendis velit similique earum",
-//     "email": "Jayne_Kuhic@sydney.com",
-//     "body": "est natus enim nihil est dolore omnis voluptatem numquam\net omnis occaecati quod ullam at\nvoluptatem error expedita pariatur\nnihil sint nostrum voluptatem reiciendis et"
-// }
